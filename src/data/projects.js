086ca0822252405d27dae35bd9333cfb3dbfb893
Raghava <arvetiraghava2005@gmail.com>
2025-11-06 15:32:03 +0530

    first haaaaa

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,13 +1,16 @@
-import image from "/src/assets/projects/career.png";
+import image from "/src/assets/projects/careerGuidance.png";
+import portfolio from "/src/assets/projects/portfolio.png";
+import stopclock from "/src/assets/projects/stopclock.png";
+import calculator from "/src/assets/projects/calculator.png";
 export default [
 {
 slug:'portfolio',
 title:'Interactive Portfolio',
-description:'A blazing-fast portfolio built with React + Vite, focus on accessibility and performance.',
-image: "/src/assets/projects/portfolio.png",
+description:'It is a personal portfolio website built using React and Vite to showcase my projects, skills, and experience in web development.',
+image: portfolio,
 tech:['React','Vite','CSS Modules'],
 github: "https://github.com/Raghava-bot",
-live: "",
+live: "https://portfolio-two-silk-46.vercel.app",
 },
 {
 slug:'career Guidance Website',
@@ -17,5 +20,23 @@ image:image,
 tech:['React','CSS','JavaScript'],
 github: "https://github.com/Raghava-bot",
 live: "https://career-guidance-website-three.vercel.app",
+},
+{
+slug:'stopclock',
+title:'stopclock App',
+description:'It is a simple stopwatch app built using HTML,CSS and Javascript that allows users to start, stop, and reset the timer.',
+image: stopclock,
+tech:['React','Vite','CSS Modules'],
+github: "https://github.com/Raghava-bot",
+live: "https://raghava-bot.github.io/Stop-Clock/",
+},
+{
+slug:'calculator',
+title:'Calculator App',
+description:'It is a simple calculator app built using HTML,CSS and Javascript that performs basic arithmetic operations like addition, subtraction, multiplication, and division.',
+image: calculator,
+tech:['React','Vite','CSS Modules'],
+github: "https://github.com/Raghava-bot",
+live: "https://raghava-bot.github.io/Calculator/",
 }
 ]
